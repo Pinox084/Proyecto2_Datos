7e721fe482587aa7ff9230ae1cb293c561909a01
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2023-06-27 23:46:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,50 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
-#include "Quad.cpp"//esto cambia dependiendo de DEV o sublime con terminal
-
+#include "Quad.h"
+#include <time.h>
+#include <chrono>
 using namespace std;
 int main()
 {
-    Quad center(Point(0, 0), Point(30000000, 30000000));
-    string linea;
+    Quad center(Point(0, 0), Point(3000000, 3000000));
+    for(int j=0; j<5;j++){
+    	string linea;
 	ifstream archivo("worldcitiespop_fixed.csv");
 	int i=0;
 	getline(archivo, linea);
-	while(getline(archivo,linea) && i<20000){
+	
+	while(getline(archivo,linea) && i<30000000){
 		stringstream stream(linea);
 		string posicion_x, posicion_y, poblacion, basura;
 		getline(stream, basura,';');
 		getline(stream, basura,';');
 		getline(stream, basura,';');
 		getline(stream, basura,';');
-		//getline(stream, basura,';');//son saltos de columnas
-			
 		getline(stream, poblacion,';');
 		getline(stream, posicion_y,';');
 		getline(stream, posicion_x,';');
-		
-		int pox;
-		int poy;
-		float delta_pox=stof(posicion_x) - stoi(posicion_x);
-		float delta_poy=stof(posicion_y) - stoi(posicion_y);
 		int population=stoi(poblacion);
-		
-		pox=stoi(posicion_x)+delta_pox*10000000;
-		poy=stoi(posicion_y)+delta_poy*10000000;
-		Node* ins1= new Node(Point(pox+180,poy+180),population);
+		srand(time(NULL));
+		float  derp = stof(posicion_x)+180;
+        float pepe = stof(posicion_y)+180;
+        derp = derp * 1000;
+        pepe = pepe * 1000;
+        int aux1 = (int)derp;
+        int aux2 = (int)pepe;
+        Point euler(aux1, aux2);
+		int num=1+rand()%(1001-1);
+		Node* ins1= new Node(euler,population);
 		center.insert(ins1);
-		cout<<"puntero: "<<ins1<<endl;
 		i++;
 	}
-	cout<<"iteraciones "<<i<<endl;
-    cout<<"TAMANO DE LA LISTA----->"<<center._list().size()<<endl;
-    cout<<"ELEMENTO DE LA LISTA 0----->"<<center._list().at(0)->data<<endl;
-    cout<<"ELEMENTO DE LA LISTA 1----->"<<center._list().at(1)->data<<endl;
+	auto start_time= chrono::high_resolution_clock::now();
+	int aux = center.aggregateRegion(Point(150,150),3000000);
+	auto end_time=chrono::high_resolution_clock::now();
+	chrono::duration<float, milli> duration = end_time - start_time;
+    cout<<"duracion: "<< duration.count()<<"ms<\n"<<endl;
+    cout<<"cantidad de poblacion en la zona:  "<< aux<<endl;
+	}
+
     return 0;
 }
